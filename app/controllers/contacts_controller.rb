dfb5a734a848c2ef9f30077a5e2be949fb6b1be5
Fred <fpaula@gmail.com>
2012-11-28 09:14:06 -0200

    Implementing some railspb suggestions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,15 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
-    respond_to { |format| format.html }
   end
 
   def create
     @contact = Contact.new(params[:contact])
-    respond_to do |format|
-      if @contact.save
-        #TODO
-        #UserMailer.contact_us(@contact).deliver
-        format.html { redirect_to contact_us_path, notice: translate('messages.contacts.notice') }
-      else
-        format.html { render action: 'new' }
-      end
+    if @contact.save
+      #TODO: UserMailer.contact_us(@contact).deliver
+      redirect_to contact_us_path, notice: translate('messages.contacts.notice')
+    else
+      render :new
     end
   end
 end
